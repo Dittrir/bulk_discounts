62a67f5ec3b976e7ec64da1519a3fb434adc276a
Robin Dittrich <robinodittrich@gmail.com>
2022-01-15 16:01:12 -0800

    Add new and create actions to bulk discount controller.

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -7,4 +7,26 @@ class BulkDiscountsController < ApplicationController
   def show
     @discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.new(discount_params)
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.new(discount_params)
+      if @bulk_discount.valid?
+        @bulk_discount.save
+
+        redirect_to "/merchants/#{@merchant.id}/bulk_discounts"
+      else
+        render :new
+    end
+  end
+
+  private
+  def discount_params
+    params.permit(:percent_discount, :quantity_threshold)
+  end
 end
