6c4475d6b65c7f7f55c2d654aeb0ad030add1082
Robin Dittrich <robinodittrich@gmail.com>
2022-01-15 18:55:19 -0800

    Create view tests for show page.

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe 'Bulk Discount Show Page' do
       @discount_2 = @merchant_1.bulk_discounts.create!(percent_discount: 30, quantity_threshold: 20)
       @discount_3 = @merchant_1.bulk_discounts.create!(percent_discount: 40, quantity_threshold: 30)
     end
+
+    it 'displays the bulk discounts quantity threshold and percentage discount' do
+      visit "/merchants/#{@merchant_1.id}/bulk_discounts/#{@discount_1.id}"
+
+      expect(page).to have_content("Discount #: #{@discount_1.id}")
+      expect(page).to have_content("This discount takes #{@discount_1.percent_discount}% off purchases of #{@discount_1.quantity_threshold} or more items!")
+    end
   end
 end
