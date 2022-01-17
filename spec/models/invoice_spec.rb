24eacf6719c467d39d1aa8bb5a9a49ce922db705
Robin Dittrich <robinodittrich@gmail.com>
2022-01-16 22:02:40 -0800

    Added more model tests for reference.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -32,4 +32,45 @@ RSpec.describe Invoice do
       expect(Invoice.incomplete_invoices).to eq(expected_result)
     end
   end
+
+  describe '#total_discounted_revenue' do
+    it 'applies the best discount' do
+      invoice_21 = @customer_6.invoices.create!
+      invoice_21.invoice_items.create!(item_id: @item_4.id, quantity: 20, unit_price: @item_4.unit_price, status: 0)
+
+      #20 * 312 = 6240 * .7 (with 30% off 20 or more items) = 4368.00
+
+      expect(invoice_21.total_discounted_revenue).to eq(436800)
+    end
+
+    it 'applies the same discount to all items that meet the quantity threshold' do
+      invoice_21 = @customer_6.invoices.create!
+      invoice_21.invoice_items.create!(item_id: @item_4.id, quantity: 30, unit_price: @item_4.unit_price, status: 0)
+      invoice_21.invoice_items.create!(item_id: @item_5.id, quantity: 30, unit_price: @item_5.unit_price, status: 0)
+      invoice_21.invoice_items.create!(item_id: @item_6.id, quantity: 30, unit_price: @item_6.unit_price, status: 0)
+
+      expect(invoice_21.total_discounted_revenue).to eq(676800)
+    end
+
+    it 'displays full price for reference' do
+      invoice_21 = @customer_6.invoices.create!
+      invoice_21.invoice_items.create!(item_id: @item_4.id, quantity: 9, unit_price: @item_4.unit_price, status: 0)
+      invoice_21.invoice_items.create!(item_id: @item_5.id, quantity: 9, unit_price: @item_5.unit_price, status: 0)
+      invoice_21.invoice_items.create!(item_id: @item_6.id, quantity: 9, unit_price: @item_6.unit_price, status: 0)
+
+      expect(invoice_21.total_discounted_revenue).to eq(338400)
+    end
+
+    it 'applies discount to appropriate items' do
+      invoice_21 = @customer_6.invoices.create!
+      invoice_21.invoice_items.create!(item_id: @item_4.id, quantity: 20, unit_price: @item_4.unit_price, status: 0)
+      invoice_21.invoice_items.create!(item_id: @item_5.id, quantity: 10, unit_price: @item_5.unit_price, status: 0)
+
+      #20 * 312 = 6240 * .7 (with 30% off 20 or more items) = 4368
+      #10 * 23 = 230 * .8 (with 20% off 10 or more items) = 184
+      #4368 + 184 = 4552.00
+
+      expect(invoice_21.total_discounted_revenue).to eq(455200)
+    end
+  end
 end
