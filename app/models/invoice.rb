8d24b923133582fc27903b319f92f40d919cb48c
Robin Dittrich <robinodittrich@gmail.com>
2022-01-16 18:55:03 -0800

    Construct method for total_discounted_revenue.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,6 +4,7 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchant, through: :items
+  has_many :bulk_discounts, through: :merchant
   validates :status, presence: true
   enum status: { cancelled: 0, "in progress" => 1, completed: 2, pending: 3 }
 
@@ -16,5 +17,24 @@ class Invoice < ApplicationRecord
     .where.not(invoice_items: {status: "shipped"})
     .order(created_at: :asc)
     .distinct
+  end
+
+  def total_discounted_revenue
+    best_discount = []
+    items_with_discounts_applied = []
+    bulk_discounts = self.bulk_discounts.order(:quantity_threshold)
+    if bulk_discounts.length > 0
+      bulk_discounts.map do |discount|
+        invoice_items.where('invoice_items.quantity >= ?', discount.quantity_threshold)
+                     .group(:id)
+                     .each do |invoice_item_with_discount|
+          best_discount << invoice_item_with_discount.discounted_item_price(discount.percent_discount)
+        end
+      end
+      items_with_discounts_applied << best_discount.min
+      (items_with_discounts_applied.sum).to_i
+    else
+      total_revenue
+    end
   end
 end
