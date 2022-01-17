8e55aa2364123b39be0b4149eca1aeeb9c4d1ac6
Robin Dittrich <robinodittrich@gmail.com>
2022-01-16 20:49:17 -0800

    Remove save and open page.

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -76,6 +76,5 @@ RSpec.describe "Merchant invoice show" do
     invoice_21.invoice_items.create!(item_id: @item_4.id, quantity: 9, unit_price: @item_4.unit_price, status: 0)
     invoice_21.invoice_items.create!(item_id: @item_5.id, quantity: 3, unit_price: @item_5.unit_price, status: 0)
     visit merchant_invoice_path(@merchant_1, invoice_21)
-    save_and_open_page
   end
 end
