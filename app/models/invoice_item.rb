9418e4a7d1b96bc539f3bc79a46400e93b0873ee
Robin Dittrich <robinodittrich@gmail.com>
2022-01-16 20:49:43 -0800

    Refactored discounted_item_price method.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,12 +5,11 @@ class InvoiceItem < ApplicationRecord
   validates :quantity, :unit_price, :status, presence: true
   enum status: { pending: 0, packaged: 1, shipped: 2 }
 
-  def discounted_item_price(discount_pct)
+  def discounted_item_price(discounted_percent)
     item_price = self.item.unit_price
-    discounted_pct = ((100 - discount_pct).to_f * 0.01).round(3)
-    InvoiceItem.where(item_id: self.item_id)
-               .each do |ii|
-                 ii.update_column(:unit_price, (item_price * discounted_pct))
+    percent_of_total_price = ((100 - discounted_percent).to_f * 0.01).round(3)
+    InvoiceItem.where(item_id: self.item_id).each do |ii|
+      ii.update_column(:unit_price, (item_price * percent_of_total_price))
     end
   end
 end
