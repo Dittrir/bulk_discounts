922134f91008c30b3839a2609c8579b6784ef4e4
Robin Dittrich <robinodittrich@gmail.com>
2022-01-16 18:53:56 -0800

    Create method to pull out individual item price with discount.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -4,4 +4,10 @@ class InvoiceItem < ApplicationRecord
   has_many :transactions, through: :invoice
   validates :quantity, :unit_price, :status, presence: true
   enum status: { pending: 0, packaged: 1, shipped: 2 }
+
+  def discounted_item_price(discount_pct)
+    total_revenue = self.unit_price * self.quantity
+    discount_pct = ((100 - discount_pct).to_f * 0.01).round(3)
+    discount_pct * total_revenue
+  end
 end
