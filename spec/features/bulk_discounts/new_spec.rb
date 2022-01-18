f329d1dde856fa5c0a80de251cffac5eb8150b19
Robin Dittrich <robinodittrich@gmail.com>
2022-01-17 19:27:26 -0800

    Capitalize refactor.

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -23,15 +23,15 @@ RSpec.describe 'Bulk Discounts New Page' do
     it 'sad path: gives error messages when form is submitted with empty fields' do
       visit "/merchants/#{@merchant_1.id}/bulk_discounts/new"
 
-      expect(page).to_not have_content("Discount not created: Fields can't be left blank")
+      expect(page).to_not have_content("Discount not created: Fields can't be blank")
 
       click_button('Submit')
 
       expect(current_path).to eq("/merchants/#{@merchant_1.id}/bulk_discounts")
       expect(page).to have_content("4 errors prohibited this post from being saved")
-      expect(page).to have_content("Percent discount can't be left blank.")
+      expect(page).to have_content("Percent discount can't be blank")
       expect(page).to have_content("Percent discount must be between 0 and 100")
-      expect(page).to have_content("Quantity threshold can't be left blank")
+      expect(page).to have_content("Quantity threshold can't be blank")
       expect(page).to have_content("Quantity threshold must be 2 or greater.")
     end
 
@@ -92,7 +92,7 @@ RSpec.describe 'Bulk Discounts New Page' do
       expect(current_path).to eq("/merchants/#{@merchant_1.id}/bulk_discounts")
       expect(page).to have_content("3 errors prohibited this post from being saved")
       expect(page).to have_content("Percent discount must be between 0 and 100")
-      expect(page).to have_content("Quantity threshold can't be left blank")
+      expect(page).to have_content("Quantity threshold can't be blank")
       expect(page).to have_content("Quantity threshold must be 2 or greater.")
     end
   end
