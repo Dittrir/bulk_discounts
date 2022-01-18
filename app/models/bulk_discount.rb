fe0df77dd70c2056e6992c35487f9879e5562539
Robin Dittrich <robinodittrich@gmail.com>
2022-01-17 19:28:42 -0800

    Fix validations for bulk_discounts.

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,15 +1,14 @@
 class BulkDiscount < ApplicationRecord
   validates :percent_discount,
-            :presence => {message: "can't be left blank."},
+            :presence => {message: "can't be blank"},
             :numericality => { only_integer: true,
                                greater_than: 0,
                                less_than: 100,
                                message: "must be between 0 and 100"}
   validates :quantity_threshold,
-            :presence => {message: "can't be left blank"},
+            :presence => {message: "can't be blank"},
             :numericality => {only_integer: true,
                               greater_than: 1,
                               message: "must be 2 or greater."}
   belongs_to :merchant
-
 end
