5b2e4830e018f404652719686808d9f99259adf9
Robin Dittrich <robinodittrich@gmail.com>
2022-01-18 16:59:37 -0800

    Refactor class name.

--- app/facades/bulk_discount_facade.rb
+++ app/facades/bulk_discount_facade.rb
@@ -1,6 +1,6 @@
 class BulkDiscountFacade
   def holidays
-    three_holidays = BulkDiscountService.three_upcoming
+    three_holidays = HolidayService.three_upcoming
 
     @holidays = three_holidays.map do |data|
       Holiday.new(data)
